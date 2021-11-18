60205b2877c5d57978a702dadb8128cd99569924
barkanias <88672651+barkanias@users.noreply.github.com>
2021-11-18 14:21:31 +0200

    Create test.py

---
+++ app/test.py
@@ -0,0 +1,16 @@
+import unittest
+import app
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.app.testing = True
+
+    def test_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_message(self):
+        response = self.app.get('/')
+        self.assertIn('The hostname of the container is', response.data, msg=None)
